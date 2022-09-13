01d188a032d2a3abfcd60188113dba04c1316b7e
Pe-ngo <jamespengo95@gmail.com>
2022-09-13 15:27:04 +0300

    Create Buyers Table

--- db/migrate/20220913121930_create_buyers.rb
+++ db/migrate/20220913121930_create_buyers.rb
@@ -1,4 +1,8 @@
 class CreateBuyers < ActiveRecord::Migration[6.1]
-  def change
+  def change 
+    create_table :buyers do |t|
+      t. string :name 
+      t. string :location
+    end
   end
 end
