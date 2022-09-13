54757fc9fbab9dbd6b6ea0bdb90c0274a8cdde8a
Pe-ngo <jamespengo95@gmail.com>
2022-09-13 16:58:46 +0300

    Get Items by Id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,60 @@ class ApplicationController < Sinatra::Base
 
   get '/items' do 
     items = Item.all 
-    items.to_json(include: :buyer)
+    items.to_json
   end
+
+  # get '/items/:id' do 
+  #   item = Item.find(params[:id])
+  #   item.to_json(include: buyer)
+  # end
+
+  # get '/buyers' do 
+  #   buyer = Buyer.all
+  #   buyer.to_json 
+  # end
+
+  # post '/items' do 
+  #   item = Item.create(
+  #     name: params[:name],
+  #     category: params[:category],
+  #     price: params[:price],
+  #     isincart: params[:isincart],
+  #     buyer_id: params[:buyer_id]
+  #   )
+  #   item.to_json
+  # end
+
+  # post '/buyers' do 
+  #   buyer = Buyer.create(
+  #     name: params[:name],
+  #     location: params[:location]
+  #  )
+  #  buyer.to_json
+  # end
+
+  # patch '/items/:id' do 
+  #   item = Item.find(params[:id])
+  #   item.update(
+  #     name: params[:name],
+  #     category: params[:category],
+  #     price: params[:price]
+  #   )
+  #   item.to_json
+  # end
+
+  # patch '/buyers/:id' do 
+  #   buyer = Buyer.find(params[:id])
+  #   buyer.update(
+  #     location: params[:location]
+  #   )
+  #   buyer.to_json
+  # end
+
+  # delete '/items/:id' do 
+  #   item = Item.find(params[:id])
+  #   item.destroy
+  #   item.to_json
+  # end
 
 end
